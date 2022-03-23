7a3f647ac314c91fb660640efaa550d68d4646e7
waynemorphic <wayneklyde@gmail.com>
2022-03-23 11:00:43 +0300

    Developed a program to output the customer booking dates as per their schedule

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ let d = document;
 
 $(document).ready(function(){
     $('#booking').click(function(){
+        submitBooking();
         $('#booked-date').show(function(){
             $('#booking').submit()
 
@@ -14,18 +15,27 @@ $(document).ready(function(){
     });
 
     function submitBooking(){
-        let choosenDate = $(document).getElementById('booked-date').val();
+        let choosenDate = d.getElementById('booked-date');
+        console.log('yes')
         // let submitButton = $(document).getElementById('booking').btn;
-        let bookingDate = choosenDate.getDay();
+        let bookingDate = choosenDate.getDay;
 
-        let y = choosenDate.checked || 0;
+        // let y = choosenDate.checked || 0;
+        console.log(choosenDate.value)
+        console.log(choosenDate.checked)
 
-        alert('You have scheduled a booking on ' + bookingDate)
+        if (choosenDate.value !== ''){
+            alert('You have scheduled a booking on ' + choosenDate.value)
+        }
+        else {
+            alert('No bookings scheduled');
+        }
 
 
 
-        console.log(choosenDate)
+        
         
     }
   
 
+    
