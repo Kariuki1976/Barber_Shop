e1fb5ed5000c1dc98c2acf98fe81dabd9d66cb11
Kariuki1976 <emmanuel.mwangi@student.moringaschool.com>
2022-03-24 11:06:03 +0300

    add form validation to contact us page

--- js/index.js
+++ js/index.js
@@ -2,40 +2,54 @@ console.log('I am the barber');
 
 let d = document;
 
-$(document).ready(function(){
-    $('#booking').click(function(){
+$(document).ready(function() {
+    $('#booking').click(function() {
         submitBooking();
-        $('#booked-date').show(function(){
+        $('#booked-date').show(function() {
             $('#booking').submit()
 
         });
-           
-            
-    });
-    });
 
-    function submitBooking(){
-        let choosenDate = d.getElementById('booked-date');
-        console.log('yes')
+
+    });
+});
+
+function submitBooking() {
+    let choosenDate = d.getElementById('booked-date');
+    console.log('yes')
         // let submitButton = $(document).getElementById('booking').btn;
-        let bookingDate = choosenDate.getDay;
+    let bookingDate = choosenDate.getDay;
 
-        // let y = choosenDate.checked || 0;
-        console.log(choosenDate.value)
-        console.log(choosenDate.checked)
+    // let y = choosenDate.checked || 0;
+    console.log(choosenDate.value)
+    console.log(choosenDate.checked)
 
-        if (choosenDate.value !== ''){
-            alert('You have scheduled a booking on ' + choosenDate.value)
-        }
-        else {
-            alert('No bookings scheduled');
-        }
-
-
-
-        
-        
+    if (choosenDate.value !== '') {
+        alert('You have scheduled a booking on ' + choosenDate.value)
+    } else {
+        alert('No bookings scheduled');
     }
-  
 
-    
+
+
+
+
+}
+
+//Form validation
+
+$(document).ready(function() {
+    $("form#form").submit(function(event) {
+        // event.preventDefault();
+        var name = $("input#name").val();
+        var email = $("input#email").val();
+        var message = $("textarea#subject").val();
+        if (name && email) {
+            alert(name + ", we have received your message. Thank you for considering us. We shall respond as soon as possible.Big up!!");
+        } else {
+            alert("Please enter your name and email!");
+        }
+
+    });
+
+});
